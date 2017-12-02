3d69abc8b9f68d5bae95b808159414397c47c892
Peter West <peter.g.west@gmail.com>
2017-12-02 01:55:02 +0000

    fix export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 /* jshint node: true, esversion: 6, unused: true */
 'use strict';
-import parse from './bibtex.pegjs';
-export default { parse };
+import parser from './bibtex.pegjs';
+export default parser;
